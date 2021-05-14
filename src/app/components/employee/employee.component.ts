03aa5910e863c62f9e6985d95598dc5e67e56a0b
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-05-14 12:29:22 +0200

    implemented edit and delete functionality

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -10,7 +10,9 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent implements OnInit {
-  public employees: Employee[] = [];
+  employees: Employee[] = [];
+  editEmployee: any = null;
+  deleteEmployee: any = null;
 
   constructor(private service: EmployeeService) { }
 
@@ -22,7 +24,6 @@ export class EmployeeComponent implements OnInit {
     this.service.getEmployees().subscribe(
       (response: Employee[]) => {
         this.employees = response;
-        console.log(this.employees);
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
@@ -35,12 +36,32 @@ export class EmployeeComponent implements OnInit {
     this.service.addEmployee(addForm.value).subscribe(
       (response: Employee) => {
         this.getEmployees();
+        addForm.reset();
       },
       (error: HttpErrorResponse) => {alert(error.message)}
     )
   }
 
-  public onOpenModal(mode: String): void {
+  public onUpdateEmployee(employee: Employee): void {
+    document.getElementById('add-employee-form')?.click();
+    this.service.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {alert(error.message)}
+    )
+  }
+
+  public onDeleteEmployee(id: number): void {
+    this.service.deleteEmployee(id).subscribe(
+      (response: void) => {
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {alert(error.message)}
+    )
+  }
+
+  public onOpenModal(employee: Employee, mode: String): void {
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
     button.type = 'button';
@@ -50,12 +71,24 @@ export class EmployeeComponent implements OnInit {
       button.setAttribute('data-target', '#addEmployeeModal');
     }
     if (mode === 'edit') {
+      this.editEmployee = employee;
       button.setAttribute('data-target', '#updateEmployeeModal');
     }
     if (mode === 'delete') {
+      this.deleteEmployee = employee;
       button.setAttribute('data-target', '#deleteEmployeeModal');
     }
 
+    container?.appendChild(button);
+    button.click();
+  }
+  public onAddModal(): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    button.setAttribute('data-target', '#addEmployeeModal');
     container?.appendChild(button);
     button.click();
   }
